f0d13e0e9788439eb26d779e9cd0a5fc3a6bb251
Jair <jairaceves56@gmail.com>
2021-09-19 13:04:05 -0500

    Mostramos el menu de la app

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+const { leerInput, 
+    inquirerMenu,
+     pausa
+     } = require("./helpers/inquirer")
 
 
-console.log('Hola Perros');
 
 
+const main = async () => {
+    
+    let opt;
+
+    do{
+
+        opt = await inquirerMenu();
+        console.log({opt});
+
+
+
+        if (opt !== 0)  await pausa();
+
+    }while( opt !== 0)
+}
+
+
+main();
