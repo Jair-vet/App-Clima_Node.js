36ca4cf6eb24d022e2c7727d4ed614bee8d03ea0
Jair <jairaceves56@gmail.com>
2021-09-19 13:26:05 -0500

    Creamos el Switch del menu

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
 const { leerInput, 
     inquirerMenu,
      pausa
-     } = require("./helpers/inquirer")
+     } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 
 
 
 const main = async () => {
     
+    const busquedas = new Busquedas();
     let opt;
 
     do{
 
         opt = await inquirerMenu();
+
+        switch(opt){
+            case 1:
+                // Mostrar mensaje 
+
+                // Buscar los lugares
+
+                // Seleccionar el lugar
+
+                // Datos del Clima
+
+                // Mostrar Resultados
+                console.log('\nInformación de la Ciudad\n'.green);
+            break;
+
+
+        }
         console.log({opt});
 
 
