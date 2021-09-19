ee62b2e23c88b7463c06db420ac971ba848b530d
Jair <jairaceves56@gmail.com>
2021-09-19 14:33:31 -0500

    Agregamos la url con nuestro token a la resp con AXIOS

--- models/busquedas.js
+++ models/busquedas.js
@@ -1,4 +1,4 @@
-
+const axios = require('axios');
 
 
 
@@ -13,7 +13,9 @@ class Busquedas{
 
     async ciudad( lugar = ''){
         // realizar peticion http
-        console.log(lugar);
+        // console.log('Ciudad', lugar);
+        const resp = await axios.get('https://api.mapbox.com/geocoding/v5/mapbox.places/Ot.json?access_token=pk.eyJ1IjoidmV0ZXJhbm8yMjIiLCJhIjoiY2t0cmxkaGs3MGxncTJxbDV1ZmRyeWxrMCJ9.0CGiSOcZo0tJmKmk6SVrSw&limit=5&language=es');
+        console.log(resp.data);
 
 
         return []; // retorna los lugares 
