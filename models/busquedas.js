f3953d53d1147a5d65ea7ee9094fad251822ff74
Jair <jairaceves56@gmail.com>
2021-09-19 13:23:02 -0500

    Se creo la clase Busquedas, que sirve para buscar en el menu

--- models/busquedas.js
+++ models/busquedas.js
@@ -3,5 +3,24 @@
 
 
 
+class Busquedas{
+    
+    historial = ['Madrid', 'Guadalajara', 'Singapur'];
+
+    constructor(){
+        // Leer la base de datos solo si existe
+    }
+
+    async ciudad( lugar = ''){
+        // realizar peticion http
+        console.log(lugar);
+
+
+        return []; // retorna los lugares 
+
+    }
+
+}
+
 // Solo cuando solo vamos a exporar esta clase
 module.exports = Busquedas;
