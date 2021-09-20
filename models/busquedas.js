dd1cc37e7d1483ae84abed3c57080c67e0b7cd95
Jair <jairaceves56@gmail.com>
2021-09-20 12:15:57 -0500

    Creamos la variable climaLugar con la declaracion del paramOpenWeather para poder hacer la desestructuracion del json

--- models/busquedas.js
+++ models/busquedas.js
@@ -15,6 +15,13 @@ class Busquedas{
             'language': 'es'
         }
     }
+    get paramOpenWeather(){
+        return{
+            appid: process.env.OPENWEATHER_KEY,
+            units: 'metric',
+            lang: 'es',
+        }
+    }
 
     async ciudad( lugar = ''){
 
@@ -26,16 +33,40 @@ class Busquedas{
             });
 
             const resp = await instance.get();
-            console.log(resp.data);
-
-
-            return []
+            return resp.data.features.map( lugar=> ({
+                id: lugar.id,
+                nombre: lugar.place_name,
+                lng: lugar.center[0],
+                lat: lugar.center[1],
+            }));
 
         }catch (error){
             return [];
         }
          // retorna los lugares 
 
+    }
+    async climaLugar(lat, lon){
+             
+        try{
+           const instance = axios.create({
+               baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+               params: {...this.paramOpenWeather, lat, lon}
+               
+        });
+        const resp = await instance.get();
+        const {weather, main} = resp.data;
+
+        return {
+            desc: weather[0].description,
+            min: main.temp_min,
+            max: main.temp_max,
+            temp: main.temp
+
+        }
+       }catch (error){
+           console.log(error);
+       }
     }
 
 }
