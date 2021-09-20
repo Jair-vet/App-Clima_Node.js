b03889f065eb8f124c4aca597e228bb904011d63
Jair <jairaceves56@gmail.com>
2021-09-20 12:14:28 -0500

    Agregamos la funcion de clima para ver la tem, min, max, desc

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ require('dotenv').config()
 
 const { leerInput, 
     inquirerMenu,
-     pausa
+     pausa,
+     listarLugares
      } = require("./helpers/inquirer");
 const Busquedas = require("./models/busquedas");
-
-
-
 
 const main = async () => {
     
@@ -21,30 +19,35 @@ const main = async () => {
         switch(opt){
             case 1:
                 // Mostrar mensaje 
-                const lugar = await leerInput('Ciudad: ');
-                await busquedas.ciudad(lugar);
+                const termino = await leerInput('Ciudad: ');
                 
-
                 // Buscar los lugares
-
+                const lugares = await busquedas.ciudad(termino);
+                
                 // Seleccionar el lugar
-
+                const id = await listarLugares(lugares);
+                const lugarSel = lugares.find( l => l.id === id );
+                
                 // Datos del Clima
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
 
                 // Mostrar Resultados
-                console.log('\nInformación de la Ciudad\n'.green);
-                console.log('Ciudad: ', );
-                console.log('Latitud: ', );
-                console.log('Longitud: ', );
-                console.log('Temperatura: ', );
-                console.log('Minima: ', );
-                console.log('Maxima: ', );
+                console.clear();
+                console.log('\nInformación de la Ciudad\n'.white);
+                console.log('Ciudad: '      .green, lugarSel.nombre.cyan);
+                console.log('Latitud: '     .green, lugarSel.lat);
+                console.log('Longitud: '    .green, lugarSel.lng);
+                console.log('Temperatura: ' .green, clima.temp);
+                console.log('Minima: '      .green, clima.min);
+                console.log('Maxima: '      .green, clima.max);
+                console.log('Como esta e Clima: '.green, clima.desc);
 
             break;
+         
 
 
         }
-        console.log({opt});
+        // console.log({opt});
 
 
 
